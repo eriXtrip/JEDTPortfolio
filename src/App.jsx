43a11119743f14ca3dcae34a44986f0a1e36dba3
Jon Eric Tripulca <joneric13tripulca@gmail.com>
2026-08-27 11:26:47 +0800

    added MouseEffects

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
 import { SplashScreen } from "@/components/SplashScreen";
+import MouseEffects from "@/components/originkit/ui/clickeffects";
 
 
 function App() {
@@ -20,6 +21,15 @@ function App() {
   return (
     <>
       <Toaster />
+      <div className="fixed inset-0 z-[10000] pointer-events-none">
+        <MouseEffects
+          interactionMode="burst"
+          color="#ffc01d"
+          duration={0.4}
+          showLabel={false}
+          effectSize={80}
+        />
+      </div>
       {showSplash && <SplashScreen onFinish={() => setShowSplash(false)} />}
       <BrowserRouter>
         <Routes>
